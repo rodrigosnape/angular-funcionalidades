4ccee45dcaec86309977578e9fd266de7dc50973
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-06-21 19:33:47 -0300

    44 - View Encapsulation None

--- secao03/criacao-de-um-componente/src/app/card/card.component.ts
+++ secao03/criacao-de-um-componente/src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 interface IPlano{
   infos: IInfos;
@@ -12,7 +12,9 @@ interface IInfos{
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
+  //Ninguém usa isso. Só serve para causar problemas
+  encapsulation: ViewEncapsulation.None
 })
 export class CardComponent {
   // @ts-ignore
