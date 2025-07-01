db86f40a75dec110886404a3afd71a20f85f42d0
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-01 15:51:41 -0300

    70 - Diretiva Estrutural NgIF

---
+++ secao05/NgIf/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  minhaProp = true;
+  pessoa = {
+    status: 1
+  }
+
+  toggleProp(){
+    this.minhaProp = !this.minhaProp;
+  }
+
+  isTruthy() {
+    return {};
+//     return [];
+//    return true;
+//    return 1;
+//    return "teste";
+//    return "0";
+//    return "false";
+//    return this.pessoa.status;
+  }
+
+  isFalsy(){
+  //  return false;
+  //  return 0;
+  //  return '';
+    return null;
+  //  return undefined;
+  // return -0;
+  //  return NaN;
+  //  return this.pessoa.name;
+  }
+}
