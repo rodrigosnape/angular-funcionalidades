18bde29709e58318b0d2233f86aee0f24fa1d5eb
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-03 14:58:26 -0300

    82 - Criação do Layout do Componente de Detalhes

--- secao06/filtro-lista-usuario/src/app/angular-material/angular-material.module.ts
+++ secao06/filtro-lista-usuario/src/app/angular-material/angular-material.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from "@angular/core";
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from "@angular/material/divider";
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
     imports: [
         MatButtonModule,
-        MatIconModule
+        MatIconModule,
+        MatListModule,
+        MatDividerModule
     ],
     exports: [
         MatButtonModule,
-        MatIconModule
+        MatIconModule,
+        MatListModule,
+        MatDividerModule
     ],
 })
 export class AngularMaterialModule{
