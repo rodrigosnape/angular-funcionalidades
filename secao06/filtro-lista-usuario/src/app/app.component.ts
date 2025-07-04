172a985a7bc50a8c3145d19066b8ba118c904e6c
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-04 17:27:32 -0300

    97 - Criação da Lógica de Filtro por Data

--- secao06/filtro-lista-usuario/src/app/app.component.ts
+++ secao06/filtro-lista-usuario/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { IUser } from './interfaces/user/user.interface';
 import { UsersList } from './data/users-list';
 import { IFilterOptions } from './interfaces/filter-options.interface';
 import { filter } from 'rxjs';
+import { isWithinInterval } from 'date-fns';
 
 @Component({
   selector: 'app-root',
@@ -40,7 +41,33 @@ export class AppComponent implements OnInit {
 
     filteredList = this.filterUsersListByName(filterOptions.name, usersList);
     filteredList = this.filterUsersListByStatus(filterOptions.status, filteredList);
+    filteredList = this.filterUsersListByDate(filterOptions.startDate, filterOptions.endDate, filteredList);
 
+    return filteredList;
+  }
+
+
+  filterUsersListByDate(startDate: Date | undefined, endDate: Date | undefined, usersList: IUser[]): IUser[] {
+    const DATES_NOT_SELECTED = startDate === undefined || endDate === undefined;
+
+    if(DATES_NOT_SELECTED) {
+      return usersList;
+    }
+
+    /* Opção mais elegante (?!)
+    const checkDateInterval = (user: IUser) => isWithinInterval(new Date(user.dataCadastro), {
+      start: new Date(startDate),
+      end: new Date(endDate)
+    });
+    const filteredList = usersList.filter(checkDateInterval);
+    */
+
+    //Feijão com arroz
+    const filteredList = usersList.filter((user: IUser) => isWithinInterval(new Date(user.dataCadastro), {
+      start: new Date(startDate),
+      end: new Date(endDate)
+    })); 
+    
     return filteredList;
   }
 
