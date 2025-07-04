e70d8ebdd2daf760f2709b03db51718a4b654b33
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-04 09:21:00 -0300

    85 - Passagem de Dados para o Componente de Detalhes

--- secao06/filtro-lista-usuario/src/app/app.component.ts
+++ secao06/filtro-lista-usuario/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UsersList } from './data/users-list';
+import { IUser } from './interfaces/user/user.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  userSelected: IUser = UsersList[0]; // Inicializa com o primeiro usuário da lista
 }
