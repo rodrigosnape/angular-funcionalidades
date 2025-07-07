c0d50d819f627bd59815de4e9cc6901725496974
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-07 12:22:05 -0300

    105 - Configuração static do @ViewChild

--- secao07/template-variables/src/app/app.component.ts
+++ secao07/template-variables/src/app/app.component.ts
@@ -1,20 +1,30 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { FilhoComponent } from './filho/filho.component';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit{
 
-  //Pega a instância do Componente. Diferentemente de um HTML comum
-  //Melhor é usar o @ViewChild é usar @Input e @Output
-  @ViewChild('filhoComp') filhoCompRef!:FilhoComponent;
+  //static: true   torna o ViewChild já disponível no ngOnInit. O default é false
+  //é apenas para HTML totalmente estáticos. Se tiver algum binding NÃO FUNCIONA
+  //Evitar usar esse static...
+  @ViewChild('meuInput', { static: true}) meuInput!: ElementRef<HTMLInputElement>;
 
-  hello(){
-    this.filhoCompRef.dizerOi();
-    this.filhoCompRef.message = 'Eu disse OI!!!';
+  constructor() {
+    console.log('constructor');
+  }
+
+  //Vai disparar depois de todos os binds(ngFor, ngIf) resolvidos
+  ngAfterViewInit() {
+     console.log('ngAfterViewInit', this.meuInput)
+    this.meuInput.nativeElement.focus();
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit', this.meuInput)
+    this.meuInput.nativeElement.focus();
   }
 
 }
