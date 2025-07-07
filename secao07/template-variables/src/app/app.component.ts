b5979bb4dc640c38778b0582b6331218cff42c80
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-07 14:48:28 -0300

    106 - Acessar Elementos HTML no Componente com @ViewChildren

--- secao07/template-variables/src/app/app.component.ts
+++ secao07/template-variables/src/app/app.component.ts
@@ -1,30 +1,63 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, AfterViewInit{
+export class AppComponent implements AfterViewInit {
+  buttonList = [
+    'Botão 1',
+    'Botão 2',
+    'Botão 3',
+  ]
 
-  //static: true   torna o ViewChild já disponível no ngOnInit. O default é false
-  //é apenas para HTML totalmente estáticos. Se tiver algum binding NÃO FUNCIONA
-  //Evitar usar esse static...
-  @ViewChild('meuInput', { static: true}) meuInput!: ElementRef<HTMLInputElement>;
+  @ViewChildren('meuButton') buttonsEl!: QueryList<ElementRef<HTMLButtonElement>>
 
-  constructor() {
-    console.log('constructor');
+  ngAfterViewInit(){
+    console.log(this.buttonsEl);
+
+    const primeiro = this.buttonsEl.toArray()[0];
+
+    console.log(primeiro);
+
+    primeiro.nativeElement.style.backgroundColor = 'blue';
+
+    //changes é quando a estrutura muda e NÃO a propriedade do botão
+    //se mudar a cor, por exemplo, o changes não é chamado.
+    this.buttonsEl.changes.subscribe((result) => {
+      console.log('result',result);
+    })
   }
 
-  //Vai disparar depois de todos os binds(ngFor, ngIf) resolvidos
-  ngAfterViewInit() {
-     console.log('ngAfterViewInit', this.meuInput)
-    this.meuInput.nativeElement.focus();
+  changeColor(event:Event){
+    console.log(event);
+
+    const btnElement = event.target as HTMLButtonElement;
+
+    btnElement.style.backgroundColor = 'orange';
+    btnElement.style.color = 'white';
+
   }
 
-  ngOnInit() {
-    console.log('ngOnInit', this.meuInput)
-    this.meuInput.nativeElement.focus();
+  resetButtons(){
+    this.buttonsEl.forEach((btnEl) => {
+      btnEl.nativeElement.style.backgroundColor = '';
+      btnEl.nativeElement.style.color = 'black';
+    })
   }
 
+  first(){
+    //const primeiro = this.buttonsEl.get(0);
+
+    //const primeiro = this.buttonsEl.find((btnEl) => btnEl.nativeElement.className === 'btn-0');
+
+    const primeiro = this.buttonsEl.toArray()[0];
+
+     console.log(primeiro);
+  }
+
+  remove(){
+    this.buttonList.shift();
+  }
 }
