8d5343a7d5685df0302defe8cbd6bbafeb578a70
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-07 18:40:55 -0300

    116 - Como Funciona o ElementRef em Componentes

--- secao07/projeto-element-ref/src/app/app.component.ts
+++ secao07/projeto-element-ref/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'projeto-element-ref';
+export class AppComponent implements OnInit, AfterViewInit{
+  @ViewChild('minhaDiv') divEl!: ElementRef<HTMLDivElement> 
+
+  //A Injeção de dependência do ElementRef permite que "APENAS" o conteúdo do app component seja acessado
+  constructor(private readonly _elRef: ElementRef){}
+
+  ngOnInit(){
+    console.log(this._elRef);
+    // não é possível usar getElementById
+    //_elRef.nativeElement.getElementById('#minha-outra-div')
+    const divEl = this._elRef.nativeElement.querySelector('#minha-outra-div') as HTMLDivElement;
+
+    divEl.textContent = "Sou a outra div!!!!!!!!!!";
+    divEl.style.backgroundColor = "green";
+    divEl.style.color = "white";
+    divEl.style.padding = "10px";
+    divEl.style.cursor = 'pointer';
+
+    divEl.addEventListener('click', () => console.log("clicado"));
+    console.log(divEl);
+  }
+
+  ngAfterViewInit(){
+   this.divEl.nativeElement.style.backgroundColor = 'orange'; 
+   this.divEl.nativeElement.textContent = 'Sou uma div!';
+   this.divEl.nativeElement.classList.add('minha-classe');
+  }
 }
+
+class Pessoa{
+  falar(){ console.log('oláaaaaaaaaaaaa');}
+}
+const pessoa: Pessoa = new Pessoa();
+
+pessoa.falar();
