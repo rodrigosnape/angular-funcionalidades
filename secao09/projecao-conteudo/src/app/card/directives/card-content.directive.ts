f0ca1fb80c7112491e38cb6e92fca23e1f00ae14
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-10 14:43:11 -0300

    154 - Aplicação de Diretivas nos Cards de Projeção Parte 1: Header e Content

---
+++ secao09/projecao-conteudo/src/app/card/directives/card-content.directive.ts
@@ -0,0 +1,9 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: 'app-card-content',
+  host: {'class': 'ca-c-card__content'}
+})
+export class CardContentDirective {
+
+}
---
+++ secao09/projecao-conteudo/src/app/card/directives/card-header.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: 'app-card-header',
+  host: {'class': 'ca-c-card__header'}
+
+})
+export class CardHeaderDirective {
+
+}
