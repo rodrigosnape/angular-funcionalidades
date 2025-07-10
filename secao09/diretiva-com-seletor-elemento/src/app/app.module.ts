45d53019910396e6d731127fbf54fd01e96318c5
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-09 21:39:54 -0300

    151 - Diretiva com Seletor de Elemento: Modificando Imagens

--- secao09/diretiva-com-seletor-elemento/src/app/app.module.ts
+++ secao09/diretiva-com-seletor-elemento/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { CardTextTitleDirective } from './components/card/directives/card-text-title.directive';
 import { CardTextSubtitleDirective } from './components/card/directives/card-text-subtitle.directive';
 import { CardTextDescriptionDirective } from './components/card/directives/card-text-description.directive';
+import { CardAvatarSmallDirective } from './components/card/directives/card-avatar-small.directive';
+import { CardAvatarLargeDirective } from './components/card/directives/card-avatar-large.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardTextTitleDirective,
     CardTextSubtitleDirective,
-    CardTextDescriptionDirective
+    CardTextDescriptionDirective,
+    CardAvatarSmallDirective,
+    CardAvatarLargeDirective
   ],
   imports: [
     BrowserModule,
