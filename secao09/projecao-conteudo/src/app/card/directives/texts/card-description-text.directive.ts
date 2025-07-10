1b9fde26607b71a282992c2a5faa3e9dca290783
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-10 15:09:39 -0300

    155 - Aplicação de Diretivas nos Cards de Projeção Parte 2: Textos

---
+++ secao09/projecao-conteudo/src/app/card/directives/texts/card-description-text.directive.ts
@@ -0,0 +1,9 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: 'app-card-description-text, [appCardDescriptionText]',
+  host: {'class': 'ca-c-card__description-text'}
+})
+export class CardDescriptionTextDirective {
+
+}
---
+++ secao09/projecao-conteudo/src/app/card/directives/texts/card-sub-text.directive.ts
@@ -0,0 +1,9 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: 'app-card-sub-text, [appCardSubText]',
+  host: {'class': 'ca-c-card__sub-text'}
+})
+export class CardSubTextDirective {
+
+}
