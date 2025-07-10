7787d7660e33ee3d423182842d5f5954aedb3ae6
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-10 16:48:20 -0300

    160 - Aplicação das Diretivas de Header e Content

--- secao10/projeto-cards-componentizado/src/app/components/card/directives/card-header.directive.ts
+++ secao10/projeto-cards-componentizado/src/app/components/card/directives/card-header.directive.ts
@@ -2,6 +2,6 @@ import { Directive } from '@angular/core';
 
 @Directive({
   selector: 'app-card-header',
-  host: {'class': 'ca-c-card__app-card-header'}
+  host: {'class': 'ca-c-card__header'}
 })
 export class CardHeaderDirective {}
