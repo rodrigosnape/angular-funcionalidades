88648d03a7019149cda0d9ea03bfac0932014a69
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-10 15:27:10 -0300

    156 - Aplicação de Diretivas nos Card de Projeção Parte 2: Imagens

---
+++ secao09/projecao-conteudo/src/app/card/directives/images/card-large-image.directive.ts
@@ -0,0 +1,9 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appCardLargeImage]',
+  host: {'class': 'ca-c-card__large-image'}
+})
+export class CardLargeImageDirective {
+
+}
