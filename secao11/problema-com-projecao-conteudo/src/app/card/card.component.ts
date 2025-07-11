1e2f842835b892a6aab523d2c15de07ed2a3290e
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-11 10:08:07 -0300

    172 - Projetando uma Tag de ng-template em outro Componente

--- secao11/problema-com-projecao-conteudo/src/app/card/card.component.ts
+++ secao11/problema-com-projecao-conteudo/src/app/card/card.component.ts
@@ -1,14 +1,24 @@
-import { Component, Input, TemplateRef } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, Input, OnInit, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
-export class CardComponent {
+export class CardComponent implements OnInit, AfterContentInit{
   condition: boolean = true;
 
-  @Input({required: true}) header!: TemplateRef<any>;
+/*   @Input({required: true}) header!: TemplateRef<any>;
   @Input({required: true}) content!: TemplateRef<any>;
-  @Input() footer!: TemplateRef<any>;
+  @Input() footer!: TemplateRef<any>; */
+
+  @ContentChild('meuTemplate') content!: TemplateRef<any>;
+
+  ngOnInit(){
+    console.log('OnInit', this.content)
+  }
+
+  ngAfterContentInit(){
+    console.log('AfterContentInit', this.content)
+  }
 }
