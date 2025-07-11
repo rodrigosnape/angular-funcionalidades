274480b3b27640b1d265af8c4691d62e13194be1
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-11 18:33:42 -0300

    184 - Currency Pipe: Formatação e Location

--- secao12/pipes/src/app/app.module.ts
+++ secao12/pipes/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -33,6 +33,9 @@ registerLocaleData(localePT, 'pt-BR');
     }, {
       provide: LOCALE_ID,
       useValue: 'pt-BR'
+    } , {
+      provide: DEFAULT_CURRENCY_CODE, 
+      useValue: 'BRL'
     }
   ],
   bootstrap: [AppComponent]
