379032ec40b80003b8df873dbf0e265017dc7452
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-11 12:03:44 -0300

    178 - Pipes Personalizados Parte 2: Truncate

--- secao12/pipes/src/app/app.module.ts
+++ secao12/pipes/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserStatusPipe } from './pipes/user-status.pipe';
+import { TruncatePipe } from './pipes/truncate.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserStatusPipe
+    UserStatusPipe,
+    TruncatePipe
   ],
   imports: [
     BrowserModule,
