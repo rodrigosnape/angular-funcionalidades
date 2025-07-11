61676df32c0da58b796ca5175aacc80ff8741bd8
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-11 11:21:14 -0300

    176 - Sintaxe Básica dos Pipes

--- secao12/pipes/src/app/app.component.ts
+++ secao12/pipes/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'pipes';
+  pessoa = {
+    nome: 'Rodrigo',
+    idade: 44,
+  }
 }
