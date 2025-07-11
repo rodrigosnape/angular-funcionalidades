30893003c403c9f24130fe7980fe11b2646eef4f
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-11 15:54:12 -0300

    180 - Date Pipe Parte 1: Display da Data e Fusos Horários

--- secao12/pipes/src/app/app.component.ts
+++ secao12/pipes/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 export enum UserStatusEnum {
     ATIVO = 1,
@@ -10,10 +10,27 @@ export enum UserStatusEnum {
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   pessoa = {
     nome: 'Rodrigo Veiga',
     idade: 44,
     status: UserStatusEnum.ATIVO,
+  }
+
+  //horário do UTC 0 (Z)
+  minhaData:string = '2023-10-21T21:00:00.000Z';
+
+  //horário da "minha" regição (sem o Z)
+  //minhaData:string = '2023-10-21T21:00:00.000';
+
+  minhaDataObj: Date = new Date(this.minhaData);
+
+  ngOnInit() {
+    console.log('Data convertida para região', new Date(this.minhaData));
+    console.log('Data UTC-0', new Date(this.minhaData).toUTCString());
+    
+    console.log('Timestamp', new Date(this.minhaData).getTime());
+
+    console.log('Obj',this.minhaDataObj);
   }
 }
