5aacca770ccbaec53ecfd5aef7a7650fe5a2f82b
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-11 09:45:42 -0300

    171 - Passando Template via @Input

--- secao11/problema-com-projecao-conteudo/src/app/card/card.component.ts
+++ secao11/problema-com-projecao-conteudo/src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class CardComponent {
   condition: boolean = true;
+
+  @Input({required: true}) header!: TemplateRef<any>;
+  @Input({required: true}) content!: TemplateRef<any>;
+  @Input() footer!: TemplateRef<any>;
 }
