7e95f073d1f8b4c473cbdd7cacd6c4951fc23b36
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-11 16:57:10 -0300

    182 - Date Pipe Parte 3: Location e Injection Tokens

--- secao12/pipes/src/app/app.module.ts
+++ secao12/pipes/src/app/app.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserStatusPipe } from './pipes/user-status.pipe';
 import { TruncatePipe } from './pipes/truncate.pipe';
+import { DATE_PIPE_DEFAULT_OPTIONS, DatePipeConfig } from '@angular/common';
+
+import localePT from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+const datePipeConfig: DatePipeConfig = {
+  dateFormat: 'dd/MM/YYYY',
+  timezone: '+0000'
+}
+
+registerLocaleData(localePT, 'pt-BR');
 
 @NgModule({
   declarations: [
@@ -16,7 +27,14 @@ import { TruncatePipe } from './pipes/truncate.pipe';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: DATE_PIPE_DEFAULT_OPTIONS, 
+      useValue: datePipeConfig
+    }, {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
