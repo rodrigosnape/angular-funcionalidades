5e5cbff2ccdb5b5803a8ffcb812bf5184cc5e400
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-15 19:11:15 -0300

    198 - Input e Output da Diretiva NgModel

--- secao13/ng-model-directive/src/app/app.component.ts
+++ secao13/ng-model-directive/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ng-model-directive';
+  nome: string = 'Rodrigo Veiga';
+  nome2: string = 'Rodrigo Veiga';
+
+  onChange(text:string){
+    console.warn(text);
+    this.nome = text;
+  }
+
+  show(){
+    console.log(this.nome2);
+  }
 }
