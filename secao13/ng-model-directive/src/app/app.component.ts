71c731dfed630158c27ba12321b0bf4a8d6858d4
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-15 19:42:58 -0300

    200 - Acessar a Instância do FormControl dentro da Classe do Componente

--- secao13/ng-model-directive/src/app/app.component.ts
+++ secao13/ng-model-directive/src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   nome: string = 'Rodrigo Veiga';
   nome2: string = 'Rodrigo Veiga';
 
+  @ViewChild('meuInputFormControl') inputEl!: NgModel;
+
+  ngAfterViewInit() {
+    console.log(this.inputEl);
+  }
   onChange(text:string){
     console.warn(text);
     this.nome = text;
@@ -16,5 +22,11 @@ export class AppComponent {
 
   show(){
     console.log(this.nome2);
+  }
+
+  send(){
+    if(this.inputEl.valid && this.inputEl.touched){
+      console.warn('Enviado com sucesso')
+    }
   }
 }
