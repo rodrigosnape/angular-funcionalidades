83061f7e1a2fa93648e739b9f3cfde6c6ed280a8
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-16 15:47:42 -0300

    209 - Envio (submit) das Informações do Formulário

--- secao13/ngModel-radio-button-checkbox/src/app/elements/form/form.component.ts
+++ secao13/ngModel-radio-button-checkbox/src/app/elements/form/form.component.ts
@@ -9,7 +9,35 @@ import { NgForm } from '@angular/forms';
 export class FormComponent implements AfterViewInit{
   @ViewChild('meuForm') form!: NgForm;
 
+  inputText: string ='texto inicial';
+  textarea: string = 'Descrição Inicial'
+  select: string = '1';
+  radioButton: string = '1';
+  checkbox: boolean = false;
+
   ngAfterViewInit() {
     console.log(this.form)
+  }
+
+  onSubmit(form: NgForm){
+     console.log('onSubmit()',form.value);
+
+     //form.value['nome'] = 'Rodrigo Veiga';
+      //console.log(this.inputText);
+  }
+
+  onReset(form: NgForm){
+    form.reset();
+     console.log('onReset())',form.value);
+     
+     console.log('onReset())',this.inputText);
+     console.log('onReset())',this.textarea);
+     console.log('onReset())',this.select);
+     console.log('onReset())',this.radioButton);
+     console.log('onReset())',this.checkbox);
+
+     setTimeout(() => console.log(form.value),1000);
+
+
   }
 }
