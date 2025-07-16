d08455890bd1b87576784842fc91f602ffcbac22
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-16 10:20:54 -0300

    204 - Diretiva NgModel com Elemento de TextArea

--- secao13/ng-model-directive/src/app/app.module.ts
+++ secao13/ng-model-directive/src/app/app.module.ts
@@ -4,10 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { TextareaComponent } from './elements/textarea/textarea.component';
+import { InputTextComponent } from './elements/input-text/input-text.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextareaComponent,
+    InputTextComponent    
+
   ],
   imports: [
     BrowserModule,
