b31338ec5db900a39235175ff0d1885337ef0eb6
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-21 18:34:22 -0300

    240 - Criação do Campo de E-mail

--- secao14/projeto-cadastro-usuarios/src/app/directives/directives.module.ts
+++ secao14/projeto-cadastro-usuarios/src/app/directives/directives.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from "@angular/core";
+import { EmailPatternValidatorDirective } from './email-pattern-validator.directive';
 
 @NgModule({
-    declarations: [],
-    exports: []
+    declarations: [
+    EmailPatternValidatorDirective
+  ],
+    exports: [
+      EmailPatternValidatorDirective
+    ]
 })
 export class DirectivesModule{}
