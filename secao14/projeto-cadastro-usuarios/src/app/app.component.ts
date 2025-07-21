8cdc0d4a27131d85cce99031356d6f7d4e29fdf3
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-21 14:59:25 -0300

    229 - Criação do UsersService

--- secao14/projeto-cadastro-usuarios/src/app/app.component.ts
+++ secao14/projeto-cadastro-usuarios/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './services/users.services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'projeto-cadastro-usuarios';
+export class AppComponent implements OnInit {
+  usersList: any = [];
+
+  constructor(
+    private readonly _usersService: UsersService
+  ){}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+  getUsers() {
+    this._usersService.getUsers().subscribe((usersListResponse) => {
+      this.usersList = usersListResponse;
+      console.log(this.usersList);
+    });
+  }
 }
