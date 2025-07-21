c6fc40ded8eff52a7c54757e6de037b7f2a57723
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-21 14:41:59 -0300

    228 - ⭐ Criação dos Módulos [Base para "qualquer" projeto]

--- secao14/projeto-cadastro-usuarios/src/app/app.module.ts
+++ secao14/projeto-cadastro-usuarios/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ComponentsModule } from './components/components.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,7 +14,10 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ComponentsModule
   ],
   providers: [
     provideAnimationsAsync()
---
+++ secao14/projeto-cadastro-usuarios/src/app/components/components.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { AngularMaterialModule } from "../angular-material.module";
+import { DirectivesModule } from "../directives/directives.module";
+import { PipesModule } from "../pipes/pipes.module";
+
+
+@NgModule({
+    declarations: [],
+    imports: [
+        FormsModule,
+        BrowserModule,
+        AngularMaterialModule,
+        DirectivesModule,
+        PipesModule
+    ],
+    exports: []
+})
+
+export class ComponentsModule{}
