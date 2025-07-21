8b6ac9e3e2ba7ebde18f3d84cd6a71c7e6bc6679
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-21 15:30:20 -0300

    231 - Criação do BrazilianStatesService

--- secao14/projeto-cadastro-usuarios/src/app/app.component.ts
+++ secao14/projeto-cadastro-usuarios/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { UsersService } from './services/users.services';
+import { UsersService } from './services/users.service';
 import { GenresService } from './services/genres.service';
+import { BrazilianStatesService } from './services/brazilian-states.service';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +11,18 @@ import { GenresService } from './services/genres.service';
 export class AppComponent implements OnInit {
   usersList: any = [];
   genresList: any = [];
+  statesList: any = [];
 
   constructor(
     private readonly _usersService: UsersService,
-    private readonly _genresService: GenresService
+    private readonly _genresService: GenresService,
+    private readonly _brazilianStatesService: BrazilianStatesService
   ){}
 
   ngOnInit() {
     this.getUsers();
     this.getGenres();
+    this.getStates();
   }
   private getUsers() {
     this._usersService.getUsers().subscribe((usersListResponse) => {
@@ -31,6 +35,13 @@ export class AppComponent implements OnInit {
     this._genresService.getGenres().subscribe((genresListResponse) => {
       this.genresList = genresListResponse;
       console.log(this.genresList);
+    });
+  }
+
+  private getStates() {
+    this._brazilianStatesService.getStates().subscribe((statesListResponse) => {
+      this.statesList = statesListResponse;
+      console.log(this.statesList);
     });
   }
 }
