7f14b0aa3ec9c72c181e38a45fc1de37b74e55b0
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-22 11:10:52 -0300

    247 - enviando a Data para o Datepicker

--- secao14/projeto-cadastro-usuarios/src/app/components/user-form/user-form.component.ts
+++ secao14/projeto-cadastro-usuarios/src/app/components/user-form/user-form.component.ts
@@ -3,6 +3,7 @@ import { GenresListResponse } from '../../types/genres-list-response';
 import { StatesListResponse } from '../../types/states-list-response';
 import { IUser } from '../../interfaces/user/user.interface';
 import { getPasswordStrengthValue } from '../../utils/get-password-strength-value';
+import { convertPtBrDateToDateObj } from '../../utils/convert-pt-br-date-to-date-obj';
 
 @Component({
   selector: 'app-user-form',
@@ -12,6 +13,7 @@ import { getPasswordStrengthValue } from '../../utils/get-password-strength-valu
 export class UserFormComponent implements OnInit, OnChanges{
   passwordStrengthValue = 0;
 
+  dateValue: Date | null = null;
   minDate: Date | null = null;
   maxDate: Date | null = null;
 
@@ -28,7 +30,8 @@ export class UserFormComponent implements OnInit, OnChanges{
      //console.log('changes', changes);
      const USER_CHANGED = changes['userSelected'];
      if(USER_CHANGED){
-      this.onPasswordChange(this.userSelected.password);      
+      this.onPasswordChange(this.userSelected.password);    
+      this.setBirthDateToDatepicker(this.userSelected.birthDate);  
      }
 
   }
@@ -40,6 +43,10 @@ export class UserFormComponent implements OnInit, OnChanges{
   private setMinAndMaxDate(){
     this.minDate = new Date(new Date().getFullYear() - 100, 0, 1);
     this.maxDate = new Date();
+  }
+
+  private setBirthDateToDatepicker(birthDate: string){
+    this.dateValue = convertPtBrDateToDateObj(birthDate);
   }
 
   
---
+++ secao14/projeto-cadastro-usuarios/src/app/utils/convert-pt-br-date-to-date-obj.ts
@@ -0,0 +1,26 @@
+export const convertPtBrDateToDateObj = (date:string):Date | null => {
+    
+    if(!date){
+        return null;
+    }
+    
+    const[ day, month, year ] = date.split('/').map(Number);
+
+    console.log(day, month, year);
+
+    if(isValidDate(day,month, year)){
+        return new Date(year, month-1, day);
+    }
+
+    return null
+}
+
+const isValidDate = (day:number, month:number, year:number): boolean => {
+    const date = new Date(year, month -1, day );
+
+    return (
+        date.getDate() === day &&
+        date.getMonth() === month -1 &&
+        date.getFullYear() === year
+    )
+}
