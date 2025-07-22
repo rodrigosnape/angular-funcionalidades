2af640a4e41ce54588c39db04630659de3629b26
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-22 12:37:30 -0300

    249 - Criação do Campo de Estado

--- secao14/projeto-cadastro-usuarios/src/app/angular-material.module.ts
+++ secao14/projeto-cadastro-usuarios/src/app/angular-material.module.ts
@@ -7,6 +7,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from "@angular/material/core";
 import { MatButtonModule } from "@angular/material/button";
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
     imports: [
@@ -18,6 +19,7 @@ import { MatButtonModule } from "@angular/material/button";
         MatDatepickerModule,
         MatNativeDateModule,
         MatButtonModule,
+        MatSelectModule,
     ],
     exports: [
         MatCardModule,
@@ -27,6 +29,7 @@ import { MatButtonModule } from "@angular/material/button";
         MatDatepickerModule,
         MatNativeDateModule,
         MatButtonModule,
+        MatSelectModule,
     ]
 })
 export class AngularMaterialModule {}
