a1b3cf8a7ae08bd9d082b1f6b0be2ec31deb4ef5
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-23 18:10:19 -0300

    286 - Como Adicionar e Remover FormGroups do FormArray

--- secao15/reactive-forms/src/app/EXEMPLOS/form-array-com-group/form-array-com-group.component.ts
+++ secao15/reactive-forms/src/app/EXEMPLOS/form-array-com-group/form-array-com-group.component.ts
@@ -7,20 +7,35 @@ import { FormArray, FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrl: './form-array-com-group.component.scss'
 })
 export class FormArrayComGroupComponent {
+
   musicasForm = new FormGroup({
     musicas: new FormArray([
-      new FormGroup({
-        titulo: new FormControl('', [Validators.required]),
-        banda: new FormControl('', [Validators.required]),
-      })
+      this.criarGrupoMusica()
     ]),
   })
 
   constructor(){
     console.log(this.musicasForm);
+
+    this.musicas.valueChanges.subscribe((value) => console.log('Value Changes', value));
   }
 
   get musicas(): FormArray{
     return this.musicasForm.get('musicas') as FormArray;
+  }
+
+  adicionarMusica() {
+    this.musicas.push( this.criarGrupoMusica() );
+  }
+
+  removerMusica(musicaIndex: number) {
+    this.musicas.removeAt(musicaIndex);
+  }
+
+  private criarGrupoMusica(): FormGroup {
+    return new FormGroup({
+        titulo: new FormControl('', [Validators.required]),
+        banda: new FormControl('', [Validators.required]),
+      });
   }
 }
