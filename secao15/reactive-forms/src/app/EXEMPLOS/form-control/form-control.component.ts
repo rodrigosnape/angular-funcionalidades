75fb14bdcca12ed38d353c09f66f98843f33189f
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-23 11:19:47 -0300

    264 - Como Criar um FormControl

--- secao15/reactive-forms/src/app/EXEMPLOS/form-control/form-control.component.ts
+++ secao15/reactive-forms/src/app/EXEMPLOS/form-control/form-control.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-control',
   templateUrl: './form-control.component.html',
   styleUrl: './form-control.component.scss'
 })
-export class FormControlComponent {
+export class FormControlComponent implements OnInit{
 
+  nome = new FormControl('Rodrigo Veiga', [Validators.required]);
+
+  ngOnInit() {
+    console.log(this.nome);
+  }
+
+  mostrarStatus(){
+    console.log(this.nome);
+  }
 }
