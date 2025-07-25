4f2ae10b1627ce604a6dc132e15587aa57e56ba8
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-25 18:15:48 -0300

    320 - Criação do Layout do Componente Lista de Usuários

--- secao16/projeto-reactive-forms/src/app/components/components.module.ts
+++ secao16/projeto-reactive-forms/src/app/components/components.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from "@angular/core";
 import { AngularMaterialModule } from "../angular-material/angular-material.module";
 import { PipesModule } from "../pipes/pipes.module";
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [
+    UsersListComponent
+  ],
     imports: [
        AngularMaterialModule,
        PipesModule,
     ],
-    exports: [],
+    exports: [
+      UsersListComponent
+    ],
 })
 export class ComponentsModule {}
