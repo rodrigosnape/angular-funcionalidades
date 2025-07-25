9148fd4ff8c9039ad945fdc1191ce5669cb9b7c6
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-24 21:01:11 -0300

    312 - Criação das Interfaces e Tipagens do Endpoint de Cidade

--- secao16/projeto-reactive-forms/src/app/services/cities.service.ts
+++ secao16/projeto-reactive-forms/src/app/services/cities.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map } from "rxjs";
+import { map, Observable } from "rxjs";
+import { ICitiesResponse } from "../interfaces/cities-response/cities-response.interface";
+import { CitiesList } from "../types/cities-list";
 
 @Injectable({
     providedIn: 'root'
@@ -10,9 +12,9 @@ export class CitiesService {
         private readonly _httpClient: HttpClient
     ){}
 
-    getCities(countryName: string, stateName: string) {
+    getCities(countryName: string, stateName: string): Observable<CitiesList> {
         return this._httpClient
-            .post<any>(
+            .post<ICitiesResponse>(
                 'https://countriesnow.space/api/v0.1/countries/state/cities',
                 {
                     country: countryName,
