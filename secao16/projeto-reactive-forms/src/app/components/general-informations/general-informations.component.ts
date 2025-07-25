cf0fc1686823b15d73bf5642e567d069f0fab621
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-25 20:25:29 -0300

    326 - ⭐ Binding das Informações Gerais do Usuário

--- secao16/projeto-reactive-forms/src/app/components/general-informations/general-informations.component.ts
+++ secao16/projeto-reactive-forms/src/app/components/general-informations/general-informations.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IUser } from '../../interfaces/user/user.interface';
 
 @Component({
   selector: 'app-general-informations',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './general-informations.component.scss'
 })
 export class GeneralInformationsComponent {
-
+  //Tem que colocar o undefined,  porque as informações ainda não estão carregadas
+  @Input({required:true}) user: IUser | undefined = {} as IUser;
 }
