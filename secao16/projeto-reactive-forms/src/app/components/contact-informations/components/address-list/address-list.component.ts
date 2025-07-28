8aa2e1d4ebc2ecd61400d7c98fd3cff6474120d6
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-28 13:24:24 -0300

    335 - Criação da Lógica de Lista de Endereços

--- secao16/projeto-reactive-forms/src/app/components/contact-informations/components/address-list/address-list.component.ts
+++ secao16/projeto-reactive-forms/src/app/components/contact-informations/components/address-list/address-list.component.ts
@@ -1,11 +1,60 @@
-import { Component, Input } from '@angular/core';
+import { AddressTypeEnum } from './../../../../enums/address-type.enum';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { AddressList } from '../../../../types/address-list';
+import { IAddress } from '../../../../interfaces/user/address.interface';
+import { addressTypeDescriptionMap } from '../../../../utils/address-type-description-map';
+import { IAddressToDisplay } from '../../../../interfaces/address-to-display.interfaces';
+
 
 @Component({
   selector: 'app-address-list',
   templateUrl: './address-list.component.html',
   styleUrl: './address-list.component.scss'
 })
-export class AddressListComponent {
+export class AddressListComponent implements OnChanges {
+
+  addresListToDisplay: IAddressToDisplay[] = [];
+
   @Input({ required: true }) userAddressList: AddressList | undefined = [];
+
+    ngOnChanges(changes: SimpleChanges): void {
+      const ADDRESS_LIST_LOADED = Array.isArray(changes['userAddressList'].currentValue);
+
+      if(ADDRESS_LIST_LOADED){
+        this.prepareAddressListToDisplay();
+      }
+  }
+  prepareAddressListToDisplay() {
+    this.addresListToDisplay = [];
+
+
+
+    Object.keys(addressTypeDescriptionMap).map(Number).forEach((addressType: number) => {
+      const addressFound = this.userAddressList?.find((userAdress) => userAdress.type === addressType);
+
+      this.addresListToDisplay.push(this.returnAddressToDisplay(addressFound, addressType));
+    });
+    
+  }
+  returnAddressToDisplay(address: IAddress | undefined, addressType: number): IAddressToDisplay {
+    if(!address){
+      return {
+        typeDescription: addressTypeDescriptionMap[addressType as AddressTypeEnum],
+        type: addressType,
+        street: '-',
+        complement: '-',
+        country: '-',
+        state: '-',
+        city: '-'
+      }
+    }
+
+    //destructoring do address (...address). 
+    // Ele vai pegar todas as propriedades do address e colocar dentro do objeto retornado
+    return {
+      typeDescription: addressTypeDescriptionMap[addressType as AddressTypeEnum],
+      ...address
+    }
+  }
+
 }
---
+++ secao16/projeto-reactive-forms/src/app/utils/address-type-description-map.ts
@@ -0,0 +1,7 @@
+import { AddressTypeEnum } from "../enums/address-type.enum";
+
+    export const addressTypeDescriptionMap: { [key in AddressTypeEnum]: string } = {
+      [AddressTypeEnum.RESIDENTIAL]: 'Residencial',
+      [AddressTypeEnum.WORK]: 'Trabalho',
+      [AddressTypeEnum.ALTERNATIVE]: 'Alternativo',
+    }
