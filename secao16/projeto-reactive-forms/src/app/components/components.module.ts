53c0fd1f08faa1cc2918a5499b45c1ac410b3ca3
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-28 17:14:25 -0300

    348 - Criação do Componente de Edição de Telefones

--- secao16/projeto-reactive-forms/src/app/components/components.module.ts
+++ secao16/projeto-reactive-forms/src/app/components/components.module.ts
@@ -13,6 +13,7 @@ import { ButtonsContainerComponent } from './buttons-container/buttons-container
 import { UserInformationsContainerComponent } from './user-informations-container/user-informations-container.component';
 import { GeneralInformationsEditComponent } from './general-informations-edit/general-informations-edit.component';
 import { ContactInformationsEditComponent } from './contact-informations-edit/contact-informations-edit.component';
+import { PhoneListEditComponent } from './contact-informations-edit/components/phone-list-edit/phone-list-edit.component';
 
 @NgModule({
     declarations: [
@@ -27,6 +28,7 @@ import { ContactInformationsEditComponent } from './contact-informations-edit/co
     UserInformationsContainerComponent,
     GeneralInformationsEditComponent,
     ContactInformationsEditComponent,
+    PhoneListEditComponent,
   ],
     imports: [
        AngularMaterialModule,
---
+++ secao16/projeto-reactive-forms/src/app/components/contact-informations-edit/components/phone-list-edit/phone-list-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-phone-list-edit',
+  templateUrl: './phone-list-edit.component.html',
+  styleUrl: './phone-list-edit.component.scss'
+})
+export class PhoneListEditComponent {
+
+}
