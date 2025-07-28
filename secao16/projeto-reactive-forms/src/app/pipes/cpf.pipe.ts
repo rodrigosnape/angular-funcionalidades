260af0f3bc852bb1a484991846fdf0390332dd19
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-28 14:56:06 -0300

    339 - Criação do Pipe de CPF

---
+++ secao16/projeto-reactive-forms/src/app/pipes/cpf.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'cpf'
+})
+export class CpfPipe implements PipeTransform {
+
+  transform(cpf: number, hideNumbers: boolean = false): string {
+    let formattedCpf = 
+      cpf.toString()
+      .replace(
+        /(\d{3})(\d{3})(\d{3})(\d{2})/,
+        '$1.$2.$3-$4'
+      );
+
+    if(hideNumbers) {
+      formattedCpf = 'XXX.' + formattedCpf.substring(4, 11) + '-XX';
+    }
+
+    return formattedCpf;
+  }
+
+}
