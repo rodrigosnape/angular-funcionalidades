4f2b28ff9c4221b605b534b9d348c68fa4ceacf8
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-29 09:20:06 -0300

    351 - Criação do Componente de Edição de Dependentes

---
+++ secao16/projeto-reactive-forms/src/app/components/dependents-list-edit/dependents-list-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dependents-list-edit',
+  templateUrl: './dependents-list-edit.component.html',
+  styleUrl: './dependents-list-edit.component.scss'
+})
+export class DependentsListEditComponent {
+
+}
