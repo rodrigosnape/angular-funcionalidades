78b8f4c96ae4863634c47e0d7a4551d0e5b37e3b
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-29 12:34:17 -0300

    353 - Criação do Formulário de Usuário Parte 2: Preenchimento das Listas

--- secao16/projeto-reactive-forms/src/app/components/user-informations-container/user-form-controller.ts
+++ secao16/projeto-reactive-forms/src/app/components/user-informations-container/user-form-controller.ts
@@ -1,6 +1,9 @@
+import { DependentsList } from './../../types/dependents-list';
+import { PhoneList } from './../../types/phone-list';
 import { inject } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { IUser } from "../../interfaces/user/user.interface";
+import { AddressList } from '../../types/address-list';
 
 export class UserFormController {
     userForm!: FormGroup;
@@ -15,14 +18,68 @@ export class UserFormController {
         return this.userForm.get('generalInformations') as FormGroup;
     }
 
+    get phoneList(): FormArray {
+        return this.userForm.get('contactInformations.phoneList') as FormArray;
+    }
+
+    get addressList(): FormArray {
+        return this.userForm.get('contactInformations.addressList') as FormArray;
+    }
+
+    get dependentsList(): FormArray {
+        return this.userForm.get('dependentsList') as FormArray;
+    }
+
     fulfillUserForm(user: IUser){
         this.fulfillGeneralInformation(user);
+
+        this.fulfillPhoneList(user.phoneList);
+
+        this.fulfillAddressList(user.addressList);
+
+        this.fulfillDependentsList(user.dependentsList);
+
+        console.log(this.userForm);
+    }
+
+    private fulfillDependentsList(userDependentsList: DependentsList) {
+        userDependentsList.forEach((dependent) => {
+            this.dependentsList.push(this._fb.group({
+                name: [dependent.name, Validators.required],
+                age: [dependent.age, Validators.required],
+                document: [dependent.document, Validators.required],
+            }))
+        })
+    }
+
+    private fulfillAddressList(userAddressList: AddressList) {
+        userAddressList.forEach((address) => {
+            this.addressList.push(this._fb.group({
+                type: [address.type, Validators.required],
+                street: [address.street, Validators.required],
+                complement: [address.complement, Validators.required],
+                country: [address.country, Validators.required],
+                state: [address.state, Validators.required],
+                city: [address.city, Validators.required],
+            }))
+        })
+    }
+    
+    private fulfillPhoneList(userPhoneList: PhoneList) {
+        userPhoneList.forEach((phone) => {
+            this.phoneList.push(this._fb.group({
+                type: [phone.type, Validators.required],
+                areaCode: [phone.areaCode, Validators.required],
+                internationalCode: [phone.internationalCode, Validators.required],
+                number: [phone.number, Validators.required],
+            }))
+        })
     }
 
     private fulfillGeneralInformation(user: IUser) {
         this.generalInformations?.patchValue(user);
 
-        console.log(this.userForm);
+        //console.log(this.userForm);
     }
 
     private createUserForm() {
