940c1eddcc506862009062136af2b90aae7e840c
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-29 14:56:39 -0300

    358 - Criação da Lógica de Autocomplete da Lista de Países

--- secao16/projeto-reactive-forms/src/app/components/general-informations-edit/general-informations-edit.component.ts
+++ secao16/projeto-reactive-forms/src/app/components/general-informations-edit/general-informations-edit.component.ts
@@ -1,21 +1,52 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { CountriesList } from '../../types/countries-list';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-general-informations-edit',
   templateUrl: './general-informations-edit.component.html',
   styleUrl: './general-informations-edit.component.scss'
 })
-export class GeneralInformationsEditComponent implements OnChanges{
+export class GeneralInformationsEditComponent implements OnInit,  OnChanges{
+
+  countriesListFiltered: CountriesList = [];
+ 
   @Input( {required: true} ) userForm!: FormGroup;
   @Input( {required: true} ) countriesList: CountriesList = [] ;
   
+  ngOnInit() {
+    console.log(this.userForm);
+
+    this.watchCountryFormChangesAndfilter();
+  }
+  
   ngOnChanges(changes: SimpleChanges) {
     console.log('changes',changes);
+    this.countriesListFiltered = this.countriesList;
   }
 
   get emailControl(): FormControl {
     return this.userForm.get('generalInformations.email') as FormControl;
+  }
+
+  get countryControl(): FormControl {
+    return this.userForm.get('generalInformations.country') as FormControl;
+  }
+
+  onCountrySelected(event: MatAutocompleteSelectedEvent) {
+    console.log(event.option.value);
+  }
+  
+  private watchCountryFormChangesAndfilter() {
+    //O feijão com arroz
+    /* this.countryControl.valueChanges.subscribe((value:string) => this.filterContriesList(value)); */
+    //Mais estiloso e com menos código
+    this.countryControl.valueChanges.subscribe(this.filterContriesList.bind(this));
+  }
+
+  private filterContriesList(searchTerm: string) {
+     console.log('searchTerm',searchTerm);
+     this.countriesListFiltered = this.countriesList.filter((country) => country.name?.toLowerCase().includes(searchTerm?.toLowerCase().trim()))
   }
 }
