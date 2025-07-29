fab7bd4859ef4a3aed3a400371800d1266b7de1e
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-29 18:55:48 -0300

    369 - Lógica para Aplicar as Máscaras Dinâmicas

--- secao16/projeto-reactive-forms/src/app/components/contact-informations-edit/components/phone-list-edit/phone-list-edit.component.ts
+++ secao16/projeto-reactive-forms/src/app/components/contact-informations-edit/components/phone-list-edit/phone-list-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { FormArray, FormGroup } from '@angular/forms';
+import { PhoneTypeEnum } from '../../../../enums/phone-type.enum';
 
 @Component({
   selector: 'app-phone-list-edit',
@@ -11,5 +12,16 @@ export class PhoneListEditComponent {
 
   get phoneList(): FormArray {
     return this.userForm.get('contactInformations.phoneList') as FormArray;
+  }
+
+  getPhoneMask(phoneType: number){
+    console.log('getPhoneMask');
+    const phoneMaskmap: { [key in PhoneTypeEnum]: string} = {
+      [PhoneTypeEnum.RESIDENTIAL]: '+00 00 0000-0000',
+      [PhoneTypeEnum.MOBILE]: '+00 00 00000-0000',
+      [PhoneTypeEnum.EMERGENCY]: '+00 00 0000-0000 || +00 00 00000-0000',
+    }
+
+    return phoneMaskmap[phoneType as PhoneTypeEnum];
   }
 }
