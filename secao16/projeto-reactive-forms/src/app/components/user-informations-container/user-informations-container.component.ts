dd047126a546acf837ec1380123d02097cc9ce12
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-29 12:04:14 -0300

    352 - ⭐ Criação do Formulário de Usuário Parte 1: Criação do Controller

--- secao16/projeto-reactive-forms/src/app/components/user-informations-container/user-informations-container.component.ts
+++ secao16/projeto-reactive-forms/src/app/components/user-informations-container/user-informations-container.component.ts
@@ -1,20 +1,28 @@
 import { IUser } from './../../interfaces/user/user.interface';
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { UserFormController } from './user-form-controller';
 
 @Component({
   selector: 'app-user-informations-container',
   templateUrl: './user-informations-container.component.html',
   styleUrl: './user-informations-container.component.scss'
 })
-export class UserInformationsContainerComponent implements OnChanges {
+export class UserInformationsContainerComponent extends UserFormController implements OnChanges {
 
     currentTabIndex: number = 1;
 
     @Input({ required: true }) userSelected: IUser = {} as IUser;
 
     @Input({ required: true } ) isInEditMode: boolean = false;
-    //Argumento existe, mas no momento não é utilizado. Então ele fica como _  (?)
-    ngOnChanges(_: SimpleChanges): void {
-      this.currentTabIndex = 1; // Reseta o índice da aba para a primeira aba quando o usuário selecionado mudar
+    
+    ngOnChanges(changes: SimpleChanges): void {
+      this.currentTabIndex = 0; // Reseta o índice da aba para a primeira aba quando o usuário selecionado mudar
+
+      const HAS_USER_SELECTED = changes['userSelected'] && Object.keys(changes['userSelected'].currentValue).length > 0;
+
+
+      if(HAS_USER_SELECTED) {
+        this.fulfillUserForm(this.userSelected);
+      }
     }
 }
