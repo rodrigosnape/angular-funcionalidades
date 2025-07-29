5bd2a3edc69fb844a3b2f6c85d98c8052a63b46e
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-29 13:29:08 -0300

    357 - Carregamento da Lista de Países

--- secao16/projeto-reactive-forms/src/app/components/general-informations-edit/general-informations-edit.component.ts
+++ secao16/projeto-reactive-forms/src/app/components/general-informations-edit/general-informations-edit.component.ts
@@ -1,13 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { CountriesList } from '../../types/countries-list';
 
 @Component({
   selector: 'app-general-informations-edit',
   templateUrl: './general-informations-edit.component.html',
   styleUrl: './general-informations-edit.component.scss'
 })
-export class GeneralInformationsEditComponent {
+export class GeneralInformationsEditComponent implements OnChanges{
   @Input( {required: true} ) userForm!: FormGroup;
+  @Input( {required: true} ) countriesList: CountriesList = [] ;
+  
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('changes',changes);
+  }
 
   get emailControl(): FormControl {
     return this.userForm.get('generalInformations.email') as FormControl;
--- secao16/projeto-reactive-forms/src/app/components/user-informations-container/user-informations-container.component.ts
+++ secao16/projeto-reactive-forms/src/app/components/user-informations-container/user-informations-container.component.ts
@@ -1,28 +1,44 @@
 import { IUser } from './../../interfaces/user/user.interface';
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, inject, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { UserFormController } from './user-form-controller';
+import { CountriesService } from '../../services/countries.service';
+import { take } from 'rxjs';
+import { CountriesList } from '../../types/countries-list';
 
 @Component({
   selector: 'app-user-informations-container',
   templateUrl: './user-informations-container.component.html',
   styleUrl: './user-informations-container.component.scss'
 })
-export class UserInformationsContainerComponent extends UserFormController implements OnChanges {
+export class UserInformationsContainerComponent extends UserFormController implements OnInit, OnChanges {
 
     currentTabIndex: number = 1;
+    countriesList: CountriesList = [];
+
+    private readonly _contriesService = inject(CountriesService);
 
     @Input({ required: true }) userSelected: IUser = {} as IUser;
 
     @Input({ required: true } ) isInEditMode: boolean = false;
     
+    ngOnInit(){      
+      this.getCountriesList();
+    }
+    
     ngOnChanges(changes: SimpleChanges): void {
       this.currentTabIndex = 0; // Reseta o índice da aba para a primeira aba quando o usuário selecionado mudar
-
+      
       const HAS_USER_SELECTED = changes['userSelected'] && Object.keys(changes['userSelected'].currentValue).length > 0;
-
-
+      
+      
       if(HAS_USER_SELECTED) {
         this.fulfillUserForm(this.userSelected);
       }
+    }
+
+    getCountriesList() {     
+      this._contriesService.getCountries().pipe(take(1)).subscribe((countriesList: CountriesList) => {
+        this.countriesList = countriesList;
+      })
     }
 }
