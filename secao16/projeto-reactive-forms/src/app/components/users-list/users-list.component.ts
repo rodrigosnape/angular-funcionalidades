0ece8fb5a41bd642d34aa4bb29d53af40b87604f
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-30 21:34:23 -0300

    384 - Criação da Lógica para Desabilitar os Usuários

--- secao16/projeto-reactive-forms/src/app/components/users-list/users-list.component.ts
+++ secao16/projeto-reactive-forms/src/app/components/users-list/users-list.component.ts
@@ -10,10 +10,12 @@ export class UsersListComponent {
   userSelectedIndex: number | undefined;
 
   @Input({ required: true }) usersList: UsersListResponse = [];
+  @Input({ required: true }) isInEditMode: boolean = false;
 
   @Output('onUserSelected') onUserSelectedEmitt = new EventEmitter<number>();
 
   onUserSelected(userIndex: number) {
+    if(this.isInEditMode) return;
     this.userSelectedIndex = userIndex;
     this.onUserSelectedEmitt.emit(userIndex);
   }
