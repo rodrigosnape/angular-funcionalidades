cdec0307d9fffc2e6af45aa5ea24847206b16474
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-31 15:48:10 -0300

    405 - Criação da Função de Conversão do UserForm para User: Parte 2

--- secao16/projeto-reactive-forms/src/app/utils/convert-user-form-to-user.ts
+++ secao16/projeto-reactive-forms/src/app/utils/convert-user-form-to-user.ts
@@ -1,11 +1,14 @@
-import { IUserForm, IUserFormGeneralInformations } from "../interfaces/user-form.interface";
+import { IUserForm, IUserFormGeneralInformations, IUserFormPhone } from "../interfaces/user-form.interface";
 import { IUser } from "../interfaces/user/user.interface";
+import { PhoneList } from "../types/phone-list";
 import { convertDateObjToPtBrDate } from "./convert-date-obj-to-pt-br-date";
 
 export const convertUserFormToUser = (userForm: IUserForm): IUser => {
     let newUser: Partial<IUser> = {} as IUser;
 
     newUser = {...convertGeneralInformations(userForm.generalInformations)};
+
+    newUser.phoneList = [...convertPhoneList(userForm.contactInformations.phoneList)];
 
     return newUser as IUser;
 };
@@ -20,4 +23,17 @@ const convertGeneralInformations = (generalInformations: IUserFormGeneralInforma
         monthlyIncome: generalInformations.monthlyIncome,
         birthDate: convertDateObjToPtBrDate(generalInformations.birthDate),
     }
+}
+
+const convertPhoneList = (phoneList: IUserFormPhone[]): PhoneList => {
+    const newUserPhoneList: PhoneList = phoneList.map((phone) => ({
+        type: phone.type,
+        internationalCode: phone.number.substring(0, 2),
+        areaCode: phone.number.substring(2, 4),
+        number: phone.number.substring(4),
+    }));
+
+    return newUserPhoneList;
+
+
 }
