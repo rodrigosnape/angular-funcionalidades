218d6032ca12ed4768486a4b92d9c113420d4781
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-07-31 15:55:43 -0300

    406 - Criação da Função de Conversão do UserForm para User: Parte 3

--- secao16/projeto-reactive-forms/src/app/utils/convert-user-form-to-user.ts
+++ secao16/projeto-reactive-forms/src/app/utils/convert-user-form-to-user.ts
@@ -1,5 +1,6 @@
-import { IUserForm, IUserFormGeneralInformations, IUserFormPhone } from "../interfaces/user-form.interface";
+import { IUserForm, IUserFormAddress, IUserFormGeneralInformations, IUserFormPhone } from "../interfaces/user-form.interface";
 import { IUser } from "../interfaces/user/user.interface";
+import { AddressList } from "../types/address-list";
 import { PhoneList } from "../types/phone-list";
 import { convertDateObjToPtBrDate } from "./convert-date-obj-to-pt-br-date";
 
@@ -9,6 +10,8 @@ export const convertUserFormToUser = (userForm: IUserForm): IUser => {
     newUser = {...convertGeneralInformations(userForm.generalInformations)};
 
     newUser.phoneList = [...convertPhoneList(userForm.contactInformations.phoneList)];
+
+    newUser.addressList = [...convertAddressList(userForm.contactInformations.addressList)];
 
     return newUser as IUser;
 };
@@ -34,6 +37,17 @@ const convertPhoneList = (phoneList: IUserFormPhone[]): PhoneList => {
     }));
 
     return newUserPhoneList;
+}
 
+const convertAddressList = (addressList: IUserFormAddress[]): AddressList => {
+    const newUserAddressList: AddressList  = addressList.map((address) => ({
+        type: address.type,
+        street: address.street,
+        complement: address.complement,
+        country: address.country,
+        state: address.state,
+        city: address.city,
+    }));
 
+    return newUserAddressList
 }
