7dfa1cba844e2fcd4ebad5168bebbd0cb297ddd6
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-01 19:02:36 -0300

    454 - Rota Filha com Path Vazio

--- secao18/1-roteamento-standalone/src/app/app.routes.ts
+++ secao18/1-roteamento-standalone/src/app/app.routes.ts
@@ -29,7 +29,7 @@ export const routes: Routes = [
         component: PrimeiroComponent,
         children: [
             {
-                path: 'filho-a',
+                path: '',
                 title: 'Filho A',
                 component: FilhoAComponent
             },
