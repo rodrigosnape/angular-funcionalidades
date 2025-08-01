b6a169383e0a30c8911cfdac67395538df1b93bd
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-01 17:11:56 -0300

    448 - Desafio 1: Resolução

--- secao18/1-desafio-roteamento-simples/src/app/app.routes.ts
+++ secao18/1-desafio-roteamento-simples/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'informations',
+        loadComponent: () => import('./components/informations/informations.component').then(m => m.InformationsComponent),
+    },
+     {
+        path: 'contacts',
+        loadComponent: () => import('./components/contacts/contacts.component').then(m => m.ContactsComponent),
+    }, 
+    {
+        path: 'about',
+        loadComponent: () => import('./components/about/about.component').then(m => m.AboutComponent),
+    },
+    {
+        path: 'logout',
+        loadComponent: () => import('./components/logout/logout.component').then(m => m.LogoutComponent),
+    }
+];
