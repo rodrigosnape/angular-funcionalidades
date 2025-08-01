e7a14f377f35b83e4c5586684e626100b85102e3
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-01 16:22:47 -0300

    446 - ⭐ Carregamento por Demanda com Lazy Loading

--- secao18/1-roteamento-standalone/src/app/app.routes.ts
+++ secao18/1-roteamento-standalone/src/app/app.routes.ts
@@ -7,8 +7,16 @@ import { BaseComponent } from './components/base/base.component';
     { path: 'primeiro', component: PrimeiroComponent},
     { path: 'segundo', component: SegundoComponent},
 ]; */
-export const routes: Routes = [
+/* export const routes: Routes = [
     { path: 'componentes', component: BaseComponent},
     { path: 'componentes/primeiro', component: PrimeiroComponent},
     { path: 'componentes/segundo', component: SegundoComponent},
+]; */
+export const routes: Routes = [
+    { path: 'componentes', 
+        loadComponent: () => import('./components/base/base.component').then(m => m.BaseComponent)},
+    { path: 'componentes/primeiro', 
+        loadComponent: () => import('./components/primeiro/primeiro.component').then(m => m.PrimeiroComponent)},
+    { path: 'componentes/segundo', 
+        loadComponent: () => import('./components/segundo/segundo.component').then(m => m.SegundoComponent)},
 ];
