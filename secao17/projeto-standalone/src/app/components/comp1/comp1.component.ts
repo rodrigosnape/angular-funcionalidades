c9d4366de2e4992205c9771fa4c3316a41dcad7f
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-01 13:10:53 -0300

    428 - Importação de Dependências de um Componente

--- secao17/projeto-standalone/src/app/components/comp1/comp1.component.ts
+++ secao17/projeto-standalone/src/app/components/comp1/comp1.component.ts
@@ -1,9 +1,15 @@
+import { CommonModule, LowerCasePipe, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-comp1',
   standalone: true,
-  imports: [],
+  imports: [
+    //NgIf,
+    //NgFor,
+    //LowerCasePipe,
+    CommonModule,
+  ],
   templateUrl: './comp1.component.html',
   styleUrl: './comp1.component.scss'
 })
