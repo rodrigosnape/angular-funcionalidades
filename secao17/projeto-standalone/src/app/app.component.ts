eb1e76ba5f1567dd5a816e8b87424d433811d375
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-01 14:30:58 -0300

    433 - Angular Material com Componentes Standalone

--- secao17/projeto-standalone/src/app/app.component.ts
+++ secao17/projeto-standalone/src/app/app.component.ts
@@ -2,13 +2,26 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Comp1Component } from './components/comp1/comp1.component';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AngularMaterialModule } from './angular-material/angular-material.module';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Comp1Component],
+  imports: [
+    RouterOutlet, 
+    Comp1Component,
+    FormsModule, 
+/*     MatFormFieldModule, 
+    MatInputModule, */
+    AngularMaterialModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'projeto-standalone';
+  nomeControl = new FormControl('Rodrigo', Validators.required);
 }
