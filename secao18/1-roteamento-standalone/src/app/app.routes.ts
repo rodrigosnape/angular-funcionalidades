10840fb2777f358e3ad787d9f77400ca0b6890a8
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-01 18:56:29 -0300

    453 - ⭐ Criação de Rotas Filhas

--- secao18/1-roteamento-standalone/src/app/app.routes.ts
+++ secao18/1-roteamento-standalone/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { SegundoComponent } from './components/segundo/segundo.component';
 import { BaseComponent } from './components/base/base.component';
 import { InicialComponent } from './components/inicial/inicial.component';
 import { PaginaNaoEncontradaComponent } from './components/pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { FilhoAComponent } from './components/primeiro/components/filho-a/filho-a.component';
+import { FilhoBComponent } from './components/primeiro/components/filho-b/filho-b.component';
 
 /* export const routes: Routes = [
     { path: 'primeiro', component: PrimeiroComponent},
@@ -24,7 +26,20 @@ export const routes: Routes = [
         loadComponent: () => import('./components/base/base.component').then(m => m.BaseComponent)},
     { path: 'componentes/primeiro', 
         title: 'Primeiro',
-        loadComponent: () => import('./components/primeiro/primeiro.component').then(m => m.PrimeiroComponent)},
+        component: PrimeiroComponent,
+        children: [
+            {
+                path: 'filho-a',
+                title: 'Filho A',
+                component: FilhoAComponent
+            },
+            {
+                path: 'filho-b',
+                title: 'Filho B',
+                component: FilhoBComponent
+            },
+        ]
+    },
     { path: 'componentes/segundo', 
         title: 'Segundo',
         loadComponent: () => import('./components/segundo/segundo.component').then(m => m.SegundoComponent)},
