21adff6153dab7dba363ce8f1924e64b93542af5
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-02 14:37:21 -0300

    461 - Desafio 2: Resolução Parte 3 - Criação da Rota General

--- secao18/2-desafio-plataforma-usuario/src/app/app.routes.ts
+++ secao18/2-desafio-plataforma-usuario/src/app/app.routes.ts
@@ -1,9 +1,36 @@
 import { Routes } from '@angular/router';
 import { InitialComponent } from './components/initial/initial.component';
+import { GeneralComponent } from './components/general/general.component';
+import { BasicComponent } from './components/general/components/basic/basic.component';
+import { AddressComponent } from './components/general/components/address/address.component';
+import { ContactComponent } from './components/general/components/contact/contact.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: InitialComponent,
+    },
+    {
+        path: 'general',
+        component: GeneralComponent,
+        children: [
+            {
+                path: '',
+                redirectTo:'basic',
+                pathMatch: 'full',
+            },
+            {
+                path: 'basic',
+                component: BasicComponent
+            },
+            {
+                path: 'contact',
+                component: ContactComponent,
+            },
+            {
+                path: 'address',
+                component: AddressComponent,
+            }
+        ],
     }
 ];
