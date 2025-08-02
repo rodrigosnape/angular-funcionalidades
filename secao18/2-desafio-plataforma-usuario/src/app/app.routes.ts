e2f581eed9608863038bb1893e03d89384465a1c
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-02 14:19:31 -0300

    460 - Desafio 2: Resolução Parte 2 - Criação da Rota Initial

--- secao18/2-desafio-plataforma-usuario/src/app/app.routes.ts
+++ secao18/2-desafio-plataforma-usuario/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { InitialComponent } from './components/initial/initial.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: InitialComponent,
+    }
+];
