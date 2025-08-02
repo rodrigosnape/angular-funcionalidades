8e6227836ea7e830eae18d3b3ba4362683cdc78c
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-02 16:32:09 -0300

    478 - Como Redirecionar com RouterLink Passando Parâmetro

--- secao19/roteamento-dinamico-input/src/app/components/users/users.component.ts
+++ secao19/roteamento-dinamico-input/src/app/components/users/users.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Component, inject, OnInit } from '@angular/core';
+import { UsersService, IUser } from '../../services/users.service';
+import { AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+    RouterLink,
+  ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
 
+    private readonly _usersService = inject(UsersService);
+    
+    usersList$: Observable<IUser[]> = of([]);
+
+    ngOnInit(): void {
+        this.usersList$ = this._usersService.getUsers();
+    }
 }
