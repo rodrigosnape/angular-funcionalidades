a8dc53978afdb734f734c6046a2a00211021071e
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-02 15:00:57 -0300

    463 - Desafio 2: Resolução Parte 5 - Criação da Rota Not Found

--- secao18/2-desafio-plataforma-usuario/src/app/app.routes.ts
+++ secao18/2-desafio-plataforma-usuario/src/app/app.routes.ts
@@ -7,14 +7,17 @@ import { ContactComponent } from './components/general/components/contact/contac
 import { TransactionsComponent } from './components/transactions/transactions.component';
 import { CreditComponent } from './components/transactions/components/credit/credit.component';
 import { DebitComponent } from './components/transactions/components/debit/debit.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
     {
         path: '',
+        title: 'Início',
         component: InitialComponent,
     },
     {
         path: 'general',
+        title: 'Geral',
         component: GeneralComponent,
         children: [
             {
@@ -24,20 +27,24 @@ export const routes: Routes = [
             },
             {
                 path: 'basic',
+                title: 'Básico',
                 component: BasicComponent
             },
             {
                 path: 'contact',
+                title: 'Contato',
                 component: ContactComponent,
             },
             {
                 path: 'address',
+                title: 'Endereço',
                 component: AddressComponent,
             }
         ],
     },
     {
         path: 'transactions',
+        title: 'Transações',
         component: TransactionsComponent,
         children:[
             {
@@ -47,12 +54,19 @@ export const routes: Routes = [
             },
             {
                 path: 'credit',
+                title: 'Crédito',
                 component: CreditComponent,
             },
             {
                 path: 'debit',
+                title: 'Débito',
                 component: DebitComponent,
             }
         ]
+    },
+    {
+        path: '**',
+        title: 'Página não encontrada',
+        component: NotFoundComponent,
     }
 ];
