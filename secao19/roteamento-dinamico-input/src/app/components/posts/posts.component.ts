24b1f5ab9882767164f31e07f3451a946d1863d0
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-02 16:44:11 -0300

    479 - Como Acessar o Parâmetro Estático da URL com o @Input

--- secao19/roteamento-dinamico-input/src/app/components/posts/posts.component.ts
+++ secao19/roteamento-dinamico-input/src/app/components/posts/posts.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { IPost } from './../../services/posts.service';
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { PostsService } from '../../services/posts.service';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe
+  ],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss'
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
 
+  private readonly _postsService = inject(PostsService);
+
+  @Input() userId: string = '';
+
+  postsList$: Observable<IPost[]> = of([]);
+
+  ngOnInit() {
+    console.log(this.userId);
+    this.postsList$ = this._postsService.getUserPosts(this.userId);
+  }
 }
