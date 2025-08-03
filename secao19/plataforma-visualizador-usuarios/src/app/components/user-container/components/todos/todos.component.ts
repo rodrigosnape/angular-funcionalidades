5cc73b4028bdd6d4ae2f4e63f30969c6a5e609b5
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-03 13:58:14 -0300

    498 - Resolução do Desafio 1 Parte 13: Carregamento da Lista de Todos do Usuário

--- secao19/plataforma-visualizador-usuarios/src/app/components/user-container/components/todos/todos.component.ts
+++ secao19/plataforma-visualizador-usuarios/src/app/components/user-container/components/todos/todos.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TodosListService } from '../../../../services/todos-list.service';
+import { Observable, of } from 'rxjs';
+import { TodosListResponse } from '../../../../types/todos-list-response';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+  ],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit{
+
+  todosList$: Observable<TodosListResponse> = of([]);
+  
+  private readonly _todosListService = inject(TodosListService);
+  private readonly _activatedRoute = inject(ActivatedRoute);
+
+  ngOnInit() {
+    this._activatedRoute.parent?.params.subscribe(
+      (params) => this.todosList$ = this._todosListService.getUserTodos(params['userId']),
+    );
+  }
 
 }
--- secao19/plataforma-visualizador-usuarios/src/app/services/todos-list.service.ts
+++ secao19/plataforma-visualizador-usuarios/src/app/services/todos-list.service.ts
@@ -3,13 +3,13 @@ import { inject, Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { TodosListResponse } from "../types/todos-list-response";
 
-Injectable({
+@Injectable({
     providedIn: 'root'
 })
 export class TodosListService{
     private readonly _http = inject(HttpClient);
 
     getUserTodos(userId: string): Observable<TodosListResponse> {
-        return this._http.get<TodosListResponse>(`https://jsonplaceholder.typicode.com/todos?userId=${userId}`);
+        return this._http.get<TodosListResponse>('https://jsonplaceholder.typicode.com/todos?userId=' + userId);
     }
 }
