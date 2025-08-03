4da31d425f2e8fa4182abdcf610f8be4c50cb348
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-03 19:53:35 -0300

    502 - Resolução do Desafio 1 Parte 17: Recuperando os Dados do Post Selecionado

--- secao19/plataforma-visualizador-usuarios/src/app/components/user-container/components/post/post.component.ts
+++ secao19/plataforma-visualizador-usuarios/src/app/components/user-container/components/post/post.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { IPost } from '../../../../interfaces/post.interface';
+import { Observable, of } from 'rxjs';
+import { PostsListService } from '../../../../services/posts-list.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+  ],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
 })
 export class PostComponent {
+  post$: Observable<IPost> = of({} as IPost);
 
+  private readonly _postsListService = inject(PostsListService);
+
+  @Input() set postId(value: string) {
+    this.post$ = this._postsListService.getPost(value);
+  }
 }
