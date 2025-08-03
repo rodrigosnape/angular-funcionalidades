67d596556fc7ecfc1de523b715d02da67d84b767
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-03 12:33:36 -0300

    492 - Resolução do Desafio 1 Parte 7: Criação do Método getUser no UsersListService

--- secao19/plataforma-visualizador-usuarios/src/app/services/users-list.service.ts
+++ secao19/plataforma-visualizador-usuarios/src/app/services/users-list.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { UsersListResponse } from "../types/users-list-response";
+import { IUser } from "../interfaces/user.interface";
 
 @Injectable({
     providedIn: 'root'
@@ -11,5 +12,9 @@ export class UserListService {
 
     getUsers(): Observable<UsersListResponse> {
         return this._http.get<UsersListResponse>('https://jsonplaceholder.typicode.com/users');
+    }
+
+    getUser(userId:string): Observable<IUser> {
+        return this._http.get<IUser>('https://jsonplaceholder.typicode.com/users' + userId);
     }
 }
