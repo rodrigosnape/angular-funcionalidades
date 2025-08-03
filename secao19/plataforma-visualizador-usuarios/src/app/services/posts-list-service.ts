1ba44a403139eb934ca05e56696a66c80602a6a6
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-03 12:22:10 -0300

    490 - Resolução do Desafio 1 Parte 5: Criação do PostsListService

---
+++ secao19/plataforma-visualizador-usuarios/src/app/services/posts-list-service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { PostsListResponse } from "../types/posts-list-response";
+import { IPost } from "../interfaces/post.interface";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PostsListSerice {
+    private readonly _http = inject(HttpClient);
+
+    getUserPosts(userId: string): Observable<PostsListResponse> {
+        return this._http.get<PostsListResponse>(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+    }
+
+    getPost(postId: string): Observable<IPost> {
+        return this._http.get<IPost>(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+    }
+}
