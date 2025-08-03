8538e506444b0810ba338373598cd8fa1afbc2a2
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-03 12:37:34 -0300

    493 - Resolução do Desafio 1 Parte 8: Criação da Rota de Página Não Encontrada

--- secao19/plataforma-visualizador-usuarios/src/app/app.routes.ts
+++ secao19/plataforma-visualizador-usuarios/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '**',
+        title: 'Página não encontrada',
+        component: PageNotFoundComponent
+    }
+];
