2606107787c522fee550ecbab341128bffbfaf7f
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-03 22:48:02 -0300

    508 - ⭐ Como Acessar os Query Strings Dinâmicos com o @Input e ActivatedRoute

--- secao19/query-strings-e-data/src/app/components/comp1/comp1.component.ts
+++ secao19/query-strings-e-data/src/app/components/comp1/comp1.component.ts
@@ -9,14 +9,20 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './comp1.component.scss'
 })
 export class Comp1Component implements OnInit{
-  @Input() nome: string = '';
+  /* @Input() nome: string = ''; */
+
+  @Input() set nome(value: string){
+    console.log('value ->', value);
+  }
 
   private readonly _activatedRoute = inject(ActivatedRoute);
 
   ngOnInit() {
     console.log('ngOnInit Comp1Component');
     console.log('nome ->', this.nome);
-
+    //escuta apenas 1 vez
     console.log('snapshot ->',this._activatedRoute.snapshot.queryParams);
+    //fica escuntando sempre
+    this._activatedRoute.queryParams.subscribe(params => console.log('Subscribe Params ->', params));
   }
 }
