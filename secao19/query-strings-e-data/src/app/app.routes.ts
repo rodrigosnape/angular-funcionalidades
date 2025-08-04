6367f046076c08c5d8db383594603723598f4d31
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-03 23:07:45 -0300

    511 - Como Passar e Receber Valores da Propriedade "data"

--- secao19/query-strings-e-data/src/app/app.routes.ts
+++ secao19/query-strings-e-data/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Comp1Component } from './components/comp1/comp1.component';
+import { GenericComponent } from './components/generic/generic.component';
 
 export const routes: Routes = [
     {
@@ -9,5 +10,19 @@ export const routes: Routes = [
     {
         path: 'comp2',
         component: Comp1Component
-    }
+    },
+    {
+        path: 'generic-user',
+        component: GenericComponent,
+        data: {
+            role: 'user'
+        }
+    },
+    {
+        path: 'generic-admin',
+        component: GenericComponent,
+        data: {
+            role: 'admin'
+        }
+    },
 ];
