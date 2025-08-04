a7a456682d389b4121b54451563b5051b9223507
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-03 22:04:16 -0300

    504 - Resolução do Desafio 1 Parte 19: Carregamento da Lista de Comentários do Post

--- secao19/plataforma-visualizador-usuarios/src/app/components/user-container/components/post/components/comments/comments.component.ts
+++ secao19/plataforma-visualizador-usuarios/src/app/components/user-container/components/post/components/comments/comments.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommentsListService } from '../../../../../../services/comments-list.service';
+import { CommentsListResponse } from '../../../../../../types/comments-list.response';
+import { Observable, of } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-comments',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe
+  ],
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.scss'
 })
-export class CommentsComponent {
+export class CommentsComponent implements OnInit {
 
+  commentsList$: Observable<CommentsListResponse> = of([]);
+
+  private readonly _commentsListService = inject(CommentsListService);
+  private readonly _activatedRoute = inject(ActivatedRoute);
+
+  ngOnInit() {
+    this._activatedRoute.parent?.params.subscribe(
+      (params) => this.commentsList$ = this._commentsListService.getPostComments(params['postId']),
+    )
+  }
 }
