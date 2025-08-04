09e2d9833522214e70757d1076aaa0a81b3a6339
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-03 22:28:20 -0300

    505 - Criação do Ambiente para Aplicar os Conceitos

---
+++ secao19/query-strings-e-data/src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have the 'query-strings-e-data' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('query-strings-e-data');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, query-strings-e-data');
+  });
+});
---
+++ secao19/query-strings-e-data/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { Comp1Component } from './components/comp1/comp1.component';
+
+export const routes: Routes = [
+    {
+        path: 'comp1',
+        component: Comp1Component
+    }
+];
