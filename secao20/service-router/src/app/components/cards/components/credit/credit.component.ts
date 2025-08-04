f046efc59606ae8d0f2008eb02f4eba3ecc8704c
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-04 17:07:47 -0300

    519 - Navegação entre Rotas Filhas com o Método "navigate"

--- secao20/service-router/src/app/components/cards/components/credit/credit.component.ts
+++ secao20/service-router/src/app/components/cards/components/credit/credit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-credit',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './credit.component.scss'
 })
 export class CreditComponent {
+  private readonly _activatedRoute = inject(ActivatedRoute);
+  private readonly _router = inject(Router);
 
+  redirectToDebit() {
+    /* this._router.navigate(['cards', 'credit']); */
+    this._router.navigate(['../debit'], {relativeTo: this._activatedRoute});
+  }
 }
--- secao20/service-router/src/app/components/cards/components/debit/debit.component.ts
+++ secao20/service-router/src/app/components/cards/components/debit/debit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-debit',
@@ -9,4 +10,14 @@ import { Component } from '@angular/core';
 })
 export class DebitComponent {
 
+  private readonly _activatedRoute = inject(ActivatedRoute);
+  private readonly _router = inject(Router);
+
+  redirectToCredit() {
+    //Absoluto
+    /* this._router.navigate(['cards', 'credit']); */
+    
+    //Relativo
+    this._router.navigate(['../credit'], {relativeTo: this._activatedRoute});
+  }
 }
