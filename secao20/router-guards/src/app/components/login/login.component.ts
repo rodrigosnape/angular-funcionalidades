c77b1d929054cd3a5d22abc3cf3d23f4cf3bfdc6
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-05 10:36:56 -0300

    530 - Projeto Parte 8: Criação da Tela de Login

--- secao20/router-guards/src/app/components/login/login.component.ts
+++ secao20/router-guards/src/app/components/login/login.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  private readonly _authService = inject(AuthService);
+  private readonly _router = inject(Router);
+
+  onLogin() {
+    console.log(this.loginForm.value.username, this.loginForm.value.password);
+
+    this._authService.login(this.loginForm.value.username, this.loginForm.value.password).subscribe({
+      next: (_) => {
+        this._router.navigate(['dashboard'])
+      },
+      error: (error) => {
+        console.log(error);
+        const UNAUTHORIZED_CREDENTIALS_ERROR = 401;
+
+        if(error.status === UNAUTHORIZED_CREDENTIALS_ERROR) {
+          this.loginForm.setErrors({ invalidCredentials: true });
+        } else {
+          this.loginForm.setErrors({ generalCredentialsError: true });
+        }
+      }
+    });
+  }
 }
