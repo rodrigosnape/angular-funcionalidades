0825531f620165b11e1e7f73c220b5b099ee1008
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-05 11:45:57 -0300

    534 - Projeto Parte 12: Criação da Tela de NotAuthorized

--- secao20/router-guards/src/app/components/not-authorized/not-authorized.component.ts
+++ secao20/router-guards/src/app/components/not-authorized/not-authorized.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Data, RouterLink } from '@angular/router';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-not-authorized',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './not-authorized.component.html',
   styleUrl: './not-authorized.component.scss'
 })
 export class NotAuthorizedComponent {
 
+  data$: Observable<Data> = of();
+
+  private readonly _activatedRoute = inject(ActivatedRoute);
+
+  ngOnInit(){
+    this.data$ = this._activatedRoute.data;
+  }
 }
