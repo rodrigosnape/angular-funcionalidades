ec4d399247d9c07ca811e382752ac5982cd1b5f0
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-05 12:57:12 -0300

    539 - Projeto Parte 17: Recuperando os Valores do Resolver

--- secao20/router-guards/src/app/components/general/general.component.ts
+++ secao20/router-guards/src/app/components/general/general.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Data } from '@angular/router';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-general',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './general.component.html',
   styleUrl: './general.component.scss'
 })
 export class GeneralComponent {
+data$: Observable<Data> = of({});
 
+  private readonly _activatedRoute = inject(ActivatedRoute);
+
+
+  ngOnInit(){
+    this.data$ = this._activatedRoute.data;
+  }
 }
