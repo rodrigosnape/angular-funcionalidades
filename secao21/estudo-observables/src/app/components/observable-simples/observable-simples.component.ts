4d6c2bd75259a931cbfad2fa1cb34c043b570105
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-05 15:45:55 -0300

    548 - ⭐ Observables Parte 4: Como Utilizar o Operador "map"

--- secao21/estudo-observables/src/app/components/observable-simples/observable-simples.component.ts
+++ secao21/estudo-observables/src/app/components/observable-simples/observable-simples.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { ObservableService } from './observable.service';
-import { Subscription } from 'rxjs';
+import { map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-observable-simples',
@@ -16,13 +16,33 @@ export class ObservableSimplesComponent {
   private readonly _observableService = inject(ObservableService);
 
   ngOnInit(){
-    this._observableService.obsSimples().subscribe((value) => {
+/*     this._observableService.obsSimples().subscribe((value) => {
       console.log(value);
     });
 
     this.subs = this._observableService.obsInterval().subscribe((value) =>{
       console.log(value);
-    })
+    }) */
+
+      // O Pipe também pode ser feito quando for feito o subscribe.
+      // Mas desse jeito só aqui virá "mapeado" 
+/*       this._observableService.getTodoInfos(1).pipe(
+            map((todoResponse: any) => {
+              const newTodo = {
+                id: todoResponse.id,
+                title: todoResponse.title,
+              }
+      
+              return newTodo;
+            })
+          ).subscribe((response) => {
+        console.log('Todo', response);
+      }); */
+
+
+      this._observableService.getTodoInfos(1).subscribe((response) => {
+        console.log('Todo', response);
+      });
   }
 
   unsubscribeInterval(){
--- secao21/estudo-observables/src/app/components/observable-simples/observable.service.ts
+++ secao21/estudo-observables/src/app/components/observable-simples/observable.service.ts
@@ -1,10 +1,25 @@
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { map, Observable, of } from 'rxjs';
+
+interface ITodoInfosResponse {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+interface ITodoInfo{
+    id: number,
+    title: string,
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ObservableService {
+
+  private readonly _httpClient = inject(HttpClient);
 
   obsSimples(){
 
@@ -34,5 +49,18 @@ export class ObservableService {
         clearInterval(interval);
       }
     })
+  }
+
+  getTodoInfos(id: number): Observable<ITodoInfo>{
+    return this._httpClient.get<ITodoInfosResponse>(`https://jsonplaceholder.typicode.com/todos/${id}`).pipe(
+      map((todoResponse) => {
+        const newTodo = {
+          id: todoResponse.id,
+          title: todoResponse.title,
+        }
+
+        return newTodo;
+      })
+    );
   }
 }
