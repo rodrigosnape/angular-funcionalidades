ad37550883ef5d66d87687be9c950df00838a6ab
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-06 15:01:18 -0300

    570 - ⭐ Projeto Parte 7: Criação do Middleware de Validação de Token JWT

--- secao21/projetos-usuarios/projeto-usuarios-back/server.js
+++ secao21/projetos-usuarios/projeto-usuarios-back/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const { USERS_LIST_BD } = require('./utils/users-list-bd');
 const { generateTokenOnLogin, validateToken } = require('./utils/jwt-manager');
+const { authenticateToken } = require('./middlewares/authenticate-token');
 
 const app = express();
 const PORT = 3000;
@@ -29,16 +30,8 @@ app.post('/login', (req, res) => {
 
 });
 
-app.post('/validate-token', (req, res) => {
-    const authHeader = req.headers['authorization'];
-
-    console.log('authHeader: ', authHeader);
-
-    const validToken = validateToken(authHeader);
-
-    console.log('Token válido: ', validToken);
-
-    res.json({});
+app.post('/validate-token', authenticateToken, (req, res) => {
+    res.json({ message: 'Token Válido'});
 })
 
 app.listen(PORT, () => {
