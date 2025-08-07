030441bd37265290af8d7884c702699072ab90f2
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-08-07 11:43:08 -0300

    586 - Projeto Parte 21: Criação do Endpoint de Criação do Usuário

--- secao21/projetos-usuarios/projeto-usuarios-back/server.js
+++ secao21/projetos-usuarios/projeto-usuarios-back/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { USERS_LIST_BD } = require('./utils/users-list-bd');
-const { generateTokenOnLogin, validateToken } = require('./utils/jwt-manager');
+const { generateTokenOnLogin } = require('./utils/jwt-manager');
 const { authenticateToken } = require('./middlewares/authenticate-token');
 
 const app = express();
@@ -17,50 +17,75 @@ app.use(cors());
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
-    const USER_FOUND = USERS_LIST_BD.find(user => user.username === username && user.password === password);
+    const USER_FOUND = 
+        USERS_LIST_BD.find(user => user.username === username && user.password === password);
 
-    if(!USER_FOUND){
+    if(!USER_FOUND) {
         return res.status(401).json({ message: 'Invalid credentials.' });
     }
 
-    // gerar o token
     const userToken = generateTokenOnLogin(username);
 
-    return res.json( { token: userToken } );
-
+    return res.json({ token: userToken });
 });
 
 app.put('/update-user', authenticateToken, (req, res) => {
-    const tokenUserName = req.username;
+    const tokenUsername = req.username;
     const newUserInfos = req.body;
 
     const { name, email, username, password } = newUserInfos;
 
-    console.log('--->', name);
-
-    if(!name || !email || !username || !password){
-        return res.status(400).json({ message: 'All fields (name, email, username, password) are required' });
+    if(!name || !email || !username || !password) {
+        return res.status(400).json({ message: 'All fields (name, email, username, password) are required.' });
     }
 
-    const USER_FOUND = USERS_LIST_BD.findIndex((user) => user.username === tokenUserName);
+    const USER_FOUND = USERS_LIST_BD.findIndex((user) => user.username === tokenUsername);
 
-    if(USER_FOUND === -1){
-        return res.status(403).json({ message: 'User not found'});
+    if(USER_FOUND === -1) {
+        return res.status(403).json({ message: 'User not found.' });
     }
 
     USERS_LIST_BD[USER_FOUND] = newUserInfos;
 
-    const newToken = generateTokenOnLogin( username);
+    const newToken = generateTokenOnLogin(username);
 
-    return res.status(200).json({ 
+    return res.status(200).json({
         message: 'User updated successfully.',
         token: newToken,
     });
 });
 
+app.post('/create-user', authenticateToken, (req, res) => {
+    const tokenUsername = req.username;
+    const newUser = req.body;
+
+    const { name, email, username, password } = newUser;
+
+    if(!name || !email || !username || !password) {
+        return res.status(400).json({ message: 'All fields (name, email, username, password) are required.' });
+    }
+    
+    const USER_TOKEN_FOUND = USERS_LIST_BD.findIndex((user) => user.username === tokenUsername);
+
+    if(USER_TOKEN_FOUND === -1) {
+        return res.status(403).json({ message: 'User not found.' });
+    }
+
+    const USER_FOUND = USERS_LIST_BD.findIndex((user) => user.username === newUser.username);
+    const USER_ALREADY_REGISTERED = USER_FOUND !== -1;
+
+    if(USER_ALREADY_REGISTERED) {
+        return res.status(409).json({ message: 'User already registered.' });
+    }
+
+    USERS_LIST_BD.push(newUser);
+
+    return res.status(201).json({ message: 'User successfully created.' });
+});
+
 app.post('/validate-token', authenticateToken, (req, res) => {
-    res.json({ message: 'Token Válido', username: req.username});
-})
+    res.json({ message: 'Token Válido', username: req.username });
+});
 
 app.listen(PORT, () => {
     console.log(`O Servidor está rodando no http://localhost:${PORT}`);
